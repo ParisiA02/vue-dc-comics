cc7356fd81566cfc926d8d3599e8a93ad73cb3b2
Andrea Parisi <andreaparisi4502@gmail.com>
2021-11-24 17:11:08 +0100

    added main component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header/>
+    <Main/>
     <Footer/>
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Footer
+    Footer,
+    Main
   }
 }
 </script>
@@ -25,9 +28,5 @@ export default {
   }
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
   }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="text-container">
+            --> Text goes here    
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .container{
+        height: 150px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        background-color: #1C1C1C;
+
+        .text-container{
+            width: 60%;
+            margin: 0 auto;
+            color: white;
+            font-size: 30px;
+        }
+    }
+</style>
