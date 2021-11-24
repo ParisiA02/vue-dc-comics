de39877308b7d0219d8bbd678a45691166758f5d
Andrea Parisi <andreaparisi4502@gmail.com>
2021-11-24 18:14:45 +0100

    added style to header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,11 @@
       </div>
     </div>
     <div class="right-header">
+      <ul>
+        <li v-for="link,i in links" :key="i">
+          <a :href="link.url" :class="link.active ? 'active' : '' ">{{link.text}}</a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
@@ -13,19 +18,93 @@
 <script>
 export default {
   name: 'Header',
+  data() {
+    return{
+      links:[
+        {
+          text: "CHARACTERS",
+          url: "#",
+          active: false
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          active: true
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          active: false
+        },
+        {
+          text: "TV",
+          url: "#",
+          active: false
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          active: false
+        },
+        {
+          text: "COLLECTIBLES",
+          url: "#",
+          active: false
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          active: false
+        },
+        {
+          text: "FANS",
+          url: "#",
+          active: false
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          active: false
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          active: false
+        }
+      ]
+    };
+  }
 }
-// data() {
-//   return{
-//     a:10,
-//   };
-// }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   header{
-    width: 100%;
-    background-color: red;
+    width: 60%;
     height: 150px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .right-header{
+
+      li{
+        display: inline;
+        margin-right: 10px;
+        list-style-type: none;
+        font-size: 20px;
+
+        a{
+          text-decoration: none;
+          color: black;
+        }
+        .active{
+          color: #0282F9;
+        }
+      }
+
+    }
+    
   }
 </style>
